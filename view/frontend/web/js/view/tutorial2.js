91c5ec79ac48c4dc1625320b2e11f713b9b97246
Niall Tiernan <n.tiernan@youwe.nl>
2019-08-13 17:00:24 +0200

    Delete seat

--- view/frontend/web/js/view/tutorial2.js
+++ view/frontend/web/js/view/tutorial2.js
@@ -3,21 +3,25 @@ define([
     'ko'
 ], function (Component, ko) {
     'use strict';
+
     return Component.extend({
 
+        defaults: {
+            foo: 'bar'
+        },
 
         availableNames: ['Niall', 'Nino', 'Patrick', 'Lennart'],
         availableMeals: [
-            { mealName: "Standard (sandwich)", price: 0 },
-            { mealName: "Premium (lobster)", price: 34.95 },
-            { mealName: "Ultimate (whole zebra)", price: 290 }
+            {mealName: "Standard (sandwich)", price: 0},
+            {mealName: "Premium (lobster)", price: 34.95},
+            {mealName: "Ultimate (whole zebra)", price: 290}
         ],
 
         initialize() {
             this._super();
             this.seats = ko.observableArray([
-                    this.getSeatReservation('Alex', this.availableMeals[0]),
-                    this.getSeatReservation('Joe', this.availableMeals[1])
+                this.getSeatReservation('Alex', this.availableMeals[0]),
+                this.getSeatReservation('Joe', this.availableMeals[1])
             ]);
         },
 
@@ -25,12 +29,23 @@ define([
             this.seats.push(this.getSeatReservation(this.getRandomName(), this.availableMeals[0]));
         },
 
+        removeSeat(seat) {
+            this.seats.remove(seat);
+        },
+
         getSeatReservation(name, initialMeal) {
-            return { name: name, meal:ko.observable(initialMeal) };
+            let self = this;
+            self.name = name;
+            self.meal = ko.observable(initialMeal);
+            self.formattedPrice = ko.computed(function () {
+                var price = self.meal().price;
+                return price ? "€" + price.toFixed(2) : "None";
+            });
+            return {name: self.name, meal: self.meal, formattedPrice: self.formattedPrice};
         },
 
         getRandomName() {
-            return this.availableNames[Math.floor(Math.random()*this.availableNames.length)]
+            return this.availableNames[Math.floor(Math.random() * this.availableNames.length)]
         },
     });
 });
